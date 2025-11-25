a0297f744241fd007ee15412cf440f2172886c78
Eyal Liebermann <eyal.liebermann@sap.com>
2025-11-25 14:17:46 +0100

    mcp stdio search working successfully

--- mcp_server/main.py
+++ mcp_server/main.py
@@ -1,19 +1,46 @@
+import requests
+from minsearch import AppendableIndex
 from fastmcp import FastMCP
+from toyaikit.tools import wrap_instance_methods
 
-mcp = FastMCP("Demo Server")
-
-
-@mcp.tool()
-def add(a: int, b: int) -> int:
-    """Add two numbers together."""
-    return a + b
+from search_tools import SearchTools
 
 
-@mcp.tool()
-def multiply(a: int, b: int) -> int:
-    """Multiply two numbers together."""
-    return a * b
+def init_index():
+    docs_url = 'https://github.com/alexeygrigorev/llm-rag-workshop/raw/main/notebooks/documents.json'
+    docs_response = requests.get(docs_url)
+    documents_raw = docs_response.json()
+
+    documents = []
+
+    for course in documents_raw:
+        course_name = course['course']
+
+        for doc in course['documents']:
+            doc['course'] = course_name
+            documents.append(doc)
+
+    index = AppendableIndex(
+        text_fields=["question", "text", "section"],
+        keyword_fields=["course"]
+    )
+
+    index.fit(documents)
+    return index
+
+
+def init_tools():
+    index = init_index()
+    return SearchTools(index)
+
+
+def init_mcp():
+    mcp = FastMCP("FAQ Server")
+    agent_tools = init_tools()
+    wrap_instance_methods(mcp.tool, agent_tools)
+    return mcp
 
 
 if __name__ == "__main__":
+    mcp = init_mcp()
     mcp.run(transport="stdio")
